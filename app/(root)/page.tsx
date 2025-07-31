7d18307a554b135657298bf5f347da743b5d8f01
mayank kumar <162879224+mayankgit22@users.noreply.github.com>
2025-07-31 15:53:12 +0530

    Update page.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,8 +2,17 @@ import Nav from '../../components/nav'
 export default function Home() {
   return (<>
   <div className=''><Nav/>
-  <h2 className='text-2xl text-center text-blue-700'>Welcome to the Admin page Where You can create Blogs
-    <br /> with your organizations so create your first organization and create your first blog</h2></div>
+ <div className="min-h-screen bg-zinc-900 text-white flex flex-col justify-center items-center text-center px-6">
+      <h1 className="text-4xl font-bold text-sky-400 mb-4 animate-fade-in">
+        Welcome to the Admin Panel 👋
+      </h1>
+      <p className="text-lg max-w-xl mb-8 animate-fade-in delay-100">
+        To get started, create your first <strong>Organization</strong>
+      </p>
+  <h2>Step:1<span> Click on the your account and create a orgnization</span></h2>
+   <h2>Step:2<span>Now again click your organization and Post some blogs</span></h2>
+   <h2>Step:3<span>Now on the blog section u get ur own orgnization url by our domain and where you can see ur all blogs</span></h2>
+    </div></div>
 
 
   </>)
